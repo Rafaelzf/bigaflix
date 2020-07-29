f1b46d0f0350766ab2c13a170265307e978c122b
Rafael Zanini Francucci <polako.rafa@gmail.com>
2020-07-29 13:48:26 -0300

    mudança de path2

--- src/Components/BannerMain/index.js
+++ src/Components/BannerMain/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import VideoIframeResponsive from "./components/VideoIframeResponsive";
+import VideoIframeResponsive from "./components/VideoIframeResponsive/index";
 import {
   BannerMainContainer,
   ContentAreaContainer,
